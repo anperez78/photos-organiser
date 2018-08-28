d78ac5b6fb919e59fbf251b43b1745c29b6c0ad0
Antonio Perez <antonio.perez@ocado.com>
2018-08-28 15:10:45 +0200

    Adding full-text searches (TextCriteria)

--- src/main/java/anperez78/photosOrganiser/domain/Media.java
+++ src/main/java/anperez78/photosOrganiser/domain/Media.java
@@ -1,6 +1,7 @@
 package anperez78.photosOrganiser.domain;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.TextIndexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.List;
@@ -14,6 +15,7 @@ public class Media {
     private String fileName;
     private String filePath;
     private MediaType mediaType;
+    @TextIndexed
     private List<String> tags;
 
     public Media(String md5HashHex, String fileName, String filePath, List<String> tags) {
--- src/main/java/anperez78/photosOrganiser/repository/ExtendedPhotoRepositoryImpl.java
+++ src/main/java/anperez78/photosOrganiser/repository/ExtendedPhotoRepositoryImpl.java
@@ -4,8 +4,9 @@ import anperez78.photosOrganiser.domain.Media;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoOperations;
-import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.TextCriteria;
+import org.springframework.data.mongodb.core.query.TextQuery;
 import org.springframework.util.Assert;
 
 import java.util.List;
@@ -25,8 +26,14 @@ public class ExtendedPhotoRepositoryImpl implements ExtendedPhotoRepository {
     public List<Media> findByTags(List<String> tags) {
 
         log.info("findByTags: " + tags);
-        Query searchQuery = new Query(Criteria.where("tags").all(tags));
+
+        TextCriteria criteria = TextCriteria
+                .forDefaultLanguage()
+                .caseSensitive(false)
+                .matchingAny(tags.toArray(new String[tags.size()]));
+        Query searchQuery = TextQuery.queryText(criteria);
         List<Media> media = operations.find(searchQuery, Media.class);
+
         log.info("media: " + media.size());
 
         return media;
